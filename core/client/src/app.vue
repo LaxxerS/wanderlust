54e9490f68641e5ccfcf6f148d6581791feb46eb
LaxxerS <laxxers@gmail.com>
2016-01-28 17:30:39 +0800

    Updated client template

--- core/client/src/app.vue
+++ core/client/src/app.vue
@@ -1,11 +1,57 @@
 <template>
+  <div class="container">
+    <div class="">
+      <input
+        type="text"
+        placeholder="Username"
+        v-model="credentials.username"
+      >
+    </div>
+    <div class="">
+      <input
+        type="password"
+        placeholder="Password"
+        v-model="credentials.password"
+      >
+    </div>
 
+    <button type="submit" name="button" @click.prevent="submit()">Login</button>
+  </div>
 </template>
 
 <script>
-
+export default {
+  data () {
+    return {
+      credentials: {
+        username: '',
+        password: ''
+      }
+    }
+  },
+  methods: {
+    submit() {
+      alert(this.credentials.username);
+    }
+  }
+}
 </script>
 
 <style>
+body, html {
+  height: 100%;
+}
+body {
+  margin: 0;
+}
+.container {
+  height: 100%;
+  padding: 0;
+  margin: 0;
+  display: flex;
+  justify-content: center;
+  flex-direction: column;
+  align-items: center;
+}
 
 </style>
